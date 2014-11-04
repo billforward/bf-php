2c1c6b6d672e0b6f8210113b2c49934b29c952a4
Alex Birch <alexander.birch@billforward.net>
2014-11-04 11:11:12 +0000

    Large progress on migration. Rewrote comments on upgrade, migration helper functions. Wrote test for migration.

--- lib/BFPHPClient/Entities/Subscription/Subscription.php
+++ lib/BFPHPClient/Entities/Subscription/Subscription.php
@@ -226,14 +226,13 @@ class Bf_Subscription extends Bf_MutableEntity {
 	//// UPGRADE/DOWNGRADE VIA AMENDMENT
 
 	/**
-	 * Changes the value of whichever Bf_PricingComponentValue corresponds to a Bf_PricingComponent whose properties match
-	 * all those provided.
+	 * Upgrades/downgrades subscription to Bf_PricingComponentValue values corresponding to Bf_PricingComponents whose properties match.
 	 * This works only for 'arrears' or 'in advance' pricing components.
 	 * @param array List of pricing component properties; array(array('name' => 'Bandwidth usage'), array('name' => 'CPU usage'))
 	 * @param array List of values to assign to respective pricing components; array(103, 2)
 	 * @param string ENUM['immediate', 'delayed'] (Default: 'immediate') When the change happens. <immediate>: Immediately, <delayed>: At the start of the next billing period
 	 * @param string ENUM['Immediate', 'Aggregated'] (Default: 'Aggregated') Subscription-charge invoicing type <Immediate>: Generate invoice straight away with this charge applied, <Aggregated>: Add this charge to next invoice
-	 * @return Bf_PricingComponentValueAmendment
+	 * @return Bf_PricingComponentValueAmendment The created upgrade amendment.
 	 */
 	public function changeValueOfPricingComponentByProperties(array $propertiesList, array $valuesList, $changeMode = 'immediate', $invoicingType = 'Aggregated') {
 		if (!is_array($propertiesList)) {
@@ -254,21 +253,20 @@ class Bf_Subscription extends Bf_MutableEntity {
 			$newValue = $valuesList[$key];
 
 			$pricingComponentValue = $this->getValueOfPricingComponentWithProperties($propertyMap);
-			$componentCharge = new Bf_ComponentChange(array(
+			$componentChange = new Bf_ComponentChange(array(
 				'logicalComponentID' => $pricingComponentValue->pricingComponentID,
 				'oldValue' => $pricingComponentValue->value,
 				'newValue' => $newValue
 			));
 
-			array_push($componentChanges, $componentCharge);
+			array_push($componentChanges, $componentChange);
 		}
 		
 		$amendment = new Bf_PricingComponentValueAmendment(array(
 			'subscriptionID' => $this->id,
 			'componentChanges' => $componentChanges,
 			'mode' => $changeMode,
-			'invoicingType' => $invoicingType,
-			'logicalComponentID' => $pricingComponentValue->pricingComponentID
+			'invoicingType' => $invoicingType
 			));
 
 		$createdAmendment = Bf_PricingComponentValueAmendment::create($amendment);
@@ -276,12 +274,11 @@ class Bf_Subscription extends Bf_MutableEntity {
 	}
 
 	/**
-	 * Changes the value of whichever Bf_PricingComponentValue corresponds to a Bf_PricingComponent whose name
-	 * matches the one provided.
+	 * Upgrades/downgrades subscription to Bf_PricingComponentValue values corresponding to named Bf_PricingComponents.
 	 * This works only for 'arrears' or 'in advance' pricing components.
 	 * @param string ENUM['immediate', 'delayed'] (Default: 'immediate') When the change happens. <immediate>: Immediately, <delayed>: At the start of the next billing period
 	 * @param string ENUM['Immediate', 'Aggregated'] (Default: 'Aggregated') Subscription-charge invoicing type <Immediate>: Generate invoice straight away with this charge applied, <Aggregated>: Add this charge to next invoice
-	 * @return Bf_PricingComponentValueAmendment
+	 * @return Bf_PricingComponentValueAmendment The created upgrade amendment.
 	 */
 	public function changeValueOfPricingComponentsByName(array $namesToValues, $changeMode = 'immediate', $invoicingType = 'Aggregated') {
 		$propertiesList = array();
@@ -300,17 +297,105 @@ class Bf_Subscription extends Bf_MutableEntity {
 	}
 
 	/**
-	 * Changes the value of whichever Bf_PricingComponentValue corresponds to a Bf_PricingComponent which recruits
-	 * the provided Bf_UnitOfMeasure.
+	 * Upgrades/downgrades subscription to Bf_PricingComponentValue values corresponding to named Bf_PricingComponents.
 	 * This works only for 'arrears' or 'in advance' pricing components.
 	 * @param array The map of pricing component names to numerical values ('Bandwidth usage' => 102)
 	 * @param string ENUM['immediate', 'delayed'] (Default: 'immediate') When the change happens. <immediate>: Immediately, <delayed>: At the start of the next billing period
 	 * @param string ENUM['Immediate', 'Aggregated'] (Default: 'Aggregated') Subscription-charge invoicing type <Immediate>: Generate invoice straight away with this charge applied, <Aggregated>: Add this charge to next invoice
-	 * @return Bf_PricingComponentValueAmendment
+	 * @return Bf_PricingComponentValueAmendment The created upgrade amendment.
 	 */
 	public function upgrade(array $namesToValues, $changeMode = 'immediate', $invoicingType = 'Aggregated') {
 		return $this->changeValueOfPricingComponentsByName($namesToValues, $changeMode, $invoicingType);
 	}
+
+	//// MIGRATE PLAN VIA AMENDMENT
+
+	/**
+	 * Migrates subscription to new plan, with Bf_PricingComponentValue values corresponding to Bf_PricingComponents whose properties match.
+	 * This works only for 'arrears' or 'in advance' pricing components.
+	 * @param array List of pricing component properties; array(array('name' => 'Bandwidth usage'), array('name' => 'CPU usage'))
+	 * @param array List of values to assign to respective pricing components; array(103, 2)
+	 * @param string ENUM['Immediate', 'Aggregated'] (Default: 'Aggregated') Subscription-charge invoicing type <Immediate>: Generate invoice straight away with this charge applied, <Aggregated>: Add this charge to next invoice
+	 * @param Bf_ProductRatePlan The plan to migrate to.
+	 * @return Bf_ProductRatePlanMigrationAmendment The created migration amendment.
+	 */
+	public function migrateWithValueOfPricingComponentByProperties(array $propertiesList, array $valuesList, $invoicingType = 'Aggregated', Bf_ProductRatePlan $newPlan) {
+		if (!is_array($propertiesList)) {
+			throw new \Exception('Expected input to be an array (a list of entity property maps). Instead received: '+$propertiesList);
+		}
+
+		if (!is_array($valuesList)) {
+			throw new \Exception('Expected input to be an array (a list of integer values). Instead received: '+$valuesList);
+		}
+
+		$mappings = array();
+
+		foreach ($propertiesList as $key => $propertyMap) {
+			if (!is_array($propertyMap)) {
+				throw new \Exception('Expected each element of input array to be an array (a map of expected properties on entity, to values). Instead received: '+$propertyMap);
+			}
+
+			$newValue = $valuesList[$key];
+
+			$pricingComponent = $newPlan->getPricingComponentWithProperties($propertyMap);
+			$mapping = new Bf_PricingComponentValueMigrationAmendmentMapping(array(
+				'pricingComponentID' => $pricingComponent->id,
+				'value' => $newValue
+			));
+
+			array_push($mappings, $mapping);
+		}
+		
+		$amendment = new Bf_ProductRatePlanMigrationAmendment(array(
+			'subscriptionID' => $this->id,
+			'mappings' => $mappings,
+			'invoicingType' => $invoicingType
+			));
+
+		$createdAmendment = Bf_ProductRatePlanMigrationAmendment::create($amendment);
+		return $createdAmendment;
+	}
+
+	/**
+	 * Migrates subscription to new plan, with Bf_PricingComponentValue values corresponding to named Bf_PricingComponents.
+	 * This works only for 'arrears' or 'in advance' pricing components.
+	 * @param string ENUM['Immediate', 'Aggregated'] (Default: 'Aggregated') Subscription-charge invoicing type <Immediate>: Generate invoice straight away with this charge applied, <Aggregated>: Add this charge to next invoice
+	 * @param Bf_ProductRatePlan The plan to migrate to.
+	 * @return Bf_ProductRatePlanMigrationAmendment The created migration amendment.
+	 */
+	public function migrateWithValueOfPricingComponentsByName(array $namesToValues, $invoicingType = 'Aggregated', Bf_ProductRatePlan $newPlan) {
+		$propertiesList = array();
+		$valuesList = array();
+
+		foreach($namesToValues as $key => $value) {
+			// from pricing component name, create a dictionary of identifying properties
+			$pricingComponentPropertyMap = array(
+				'name' => $key
+				);
+			array_push($propertiesList, $pricingComponentPropertyMap);
+			array_push($valuesList, $value);
+		}
+
+		return $this->migrateWithValueOfPricingComponentByProperties($propertiesList, $valuesList, $invoicingType, $newPlan);
+	}
+
+	/**
+	 * Migrates subscription to new plan, with Bf_PricingComponentValue values corresponding to named Bf_PricingComponents.
+	 * This works only for 'arrears' or 'in advance' pricing components.
+	 * @param array The map of pricing component names to numerical values ('Bandwidth usage' => 102)
+	 * @param string ENUM['Immediate', 'Aggregated'] (Default: 'Aggregated') Subscription-charge invoicing type <Immediate>: Generate invoice straight away with this charge applied, <Aggregated>: Add this charge to next invoice
+	 * @param string ID of the plan to migrate to.
+	 * @param Bf_ProductRatePlan (Alternative parameter to avoid extra API request) The plan to migrate to.
+	 * @return Bf_ProductRatePlanMigrationAmendment The created migration amendment.
+	 */
+	public function migratePlan(array $namesToValues, $newPlanID = NULL, $invoicingType = 'Aggregated', Bf_ProductRatePlan $newPlan = NULL) {
+		if (is_null($newPlan)) {
+			// fetch plan for you
+			$newPlan = Bf_ProductRatePlan::getByID($newPlanID);
+		}
+
+		return $this->migrateWithValueOfPricingComponentsByName($namesToValues, $invoicingType, $newPlan);
+	}	
 
 	public static function initStatics() {
 		self::$_resourcePath = new Bf_ResourcePath('subscriptions', 'subscription');
