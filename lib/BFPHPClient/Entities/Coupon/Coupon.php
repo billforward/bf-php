c806c6cccab4adc59e60b7fb4bf41496f0bf0fa0
Alex Birch <alexander.birch@billforward.net>
2015-01-21 17:36:37 +0000

    Possibly the static construct can be retired

--- lib/BFPHPClient/Entities/Coupon/Coupon.php
+++ lib/BFPHPClient/Entities/Coupon/Coupon.php
@@ -11,9 +11,23 @@ class Bf_Coupon extends Bf_MutableEntity {
 	 * Constructs a Bf_Coupon model.
 	 * @param union[string ($id | $name) | Bf_ProductRatePlan $ratePlan] The product discounted by the coupon. <string>: ID or name of the Bf_ProductRatePlan. <Bf_ProductRatePlan>: The Bf_ProductRatePlan.
 	 * @param union[string ($id | $name) | Bf_Product $entity | NULL] (Default: NULL) The product discounted by the coupon. <string>: ID or name of the Bf_Product. <Bf_Product>: The Bf_Product. <NULL>: Refer to the product recruited by the Bf_ProductRatePlan.
-	 * @return Bf_Coupon The created coupon.
+	 * @return Bf_Coupon The created coupon model.
 	 */
 	public static function construct($ratePlan, $product = NULL) {
+		$model = new static();
+
+		$model->setRatePlan($ratePlan, $product);
+
+		return $model;
+	}
+
+	/**
+	 * Assigns to this Bf_Coupon a Bf_ProductRatePlan.
+	 * @param union[string ($id | $name) | Bf_ProductRatePlan $ratePlan] The product discounted by the coupon. <string>: ID or name of the Bf_ProductRatePlan. <Bf_ProductRatePlan>: The Bf_ProductRatePlan.
+	 * @param union[string ($id | $name) | Bf_Product $entity | NULL] (Default: NULL) The product discounted by the coupon. <string>: ID or name of the Bf_Product. <Bf_Product>: The Bf_Product. <NULL>: Refer to the product recruited by the Bf_ProductRatePlan.
+	 * @return Bf_Coupon The modified coupon model.
+	 */
+	public function setRatePlan($ratePlan, $product = NULL) {
 		if (is_null($product)) {
 			// get rate plan
 			$ratePlanEntity = Bf_ProductRatePlan::fetchIfNecessary($ratePlan);
@@ -22,12 +36,10 @@ class Bf_Coupon extends Bf_MutableEntity {
 		$productIdentifier = Bf_Product::getIdentifier($product);
 		$ratePlanIdentifier = Bf_ProductRatePlan::getIdentifier($ratePlan);
 
-		$model = new static();
+		$this->product = $product;
+		$this->productRatePlan = $ratePlan;
 
-		$model->product = $product;
-		$model->productRatePlan = $ratePlan;
-
-		return $model;
+		return $this;
 	}
 
 	/**
