7123f155de52145972313205a94a225c04c3a177
Alex Birch <alexander.birch@billforward.net>
2015-01-28 12:28:56 +0000

    Explain

--- test/BFPHPClientTest/new/config/config.example.php
+++ test/BFPHPClientTest/new/config/config.example.php
@@ -1,4 +1,8 @@
 <?php
+/*
+ * Copy paste the contents of this `config.example.php` to your own local `config.php` in the same directory.
+ * These test credentials should refer to a scratch account which you're happy to have filled with chuff created by tests.
+ */
 $credentials = array(
 	'urlRoot' => 'https://api-sandbox.billforward.net:443/v1/',
 	'access_token' => 'INSERT_ACCESS_TOKEN_HERE'
