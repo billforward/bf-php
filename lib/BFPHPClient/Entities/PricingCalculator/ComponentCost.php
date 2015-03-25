8886366ebaa10b6b326bf4f53d4b12b3ff889f97
Alex Birch <alexander.birch@billforward.net>
2015-03-25 11:53:54 +0000

    These also

---
+++ lib/BFPHPClient/Entities/PricingCalculator/ComponentCost.php
@@ -0,0 +1,27 @@
+<?php
+
+class Bf_ComponentCost extends Bf_MutableEntity {
+	public static function create(Bf_InsertableEntity $entity) {
+		throw new Bf_UnsupportedMethodException('Create support is denied for this entity; '
+		 .'at the time of writing, no API endpoint exists to support it. '
+		 .'The entity can be created through cascade only (i.e. instantiated within another entity).');
+	}
+	
+	public static function getbyID($id, $options = NULL, $customClient = NULL) {
+		throw new Bf_UnsupportedMethodException('Get by ID support is denied for this entity; '
+		 .'at the time of writing, no API endpoint exists to support it.'
+		 .'The entity can be GETted through cascade only (i.e. GET a related entity).');
+	}
+
+	public static function getAll($options = NULL, $customClient = NULL) {
+		throw new Bf_UnsupportedMethodException('Get All support is denied for this entity; '
+		 .'at the time of writing, no API endpoint exists to support it.'
+		 .'The entity can be GETted through cascade only (i.e. GET a related entity).');
+	}
+
+	public function save() {
+		throw new Bf_UnsupportedMethodException('Save support is denied for this entity; '
+		 .'at the time of writing, the provided API endpoint is not functioning.'
+		 .'The entity can be saved through cascade only (i.e. save a related entity).');
+	}
+}
