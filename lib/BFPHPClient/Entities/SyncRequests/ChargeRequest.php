9d1f8e79bb813ede94e0d3dd774eed583081cd9c
Alex Birch <alexander.birch@billforward.net>
2015-05-22 18:47:00 +0100

    ChargeRequest class

---
+++ lib/BFPHPClient/Entities/SyncRequests/ChargeRequest.php
@@ -0,0 +1,16 @@
+<?php
+
+class Bf_ChargeRequest extends Bf_BillingEntity {
+
+	public static function getByID($id, $options = NULL, $customClient = NULL) {
+		throw new Bf_UnsupportedMethodException('Get by ID support is denied for this entity; '
+		 .'at the time of writing, no API endpoint exists to support it.'
+		 .'The entity can be GETted through cascade only (i.e. GET a related entity).');
+	}
+
+	public static function getAll($options = NULL, $customClient = NULL) {
+		throw new Bf_UnsupportedMethodException('Get All support is denied for this entity; '
+		 .'at the time of writing, no API endpoint exists to support it.'
+		 .'The entity can be GETted through cascade only (i.e. GET a related entity).');
+	}
+}
