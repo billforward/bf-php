6ede7510311527e37d2f766cf5c3f2d5ab0031e9
Alex Birch <alexander.birch@billforward.net>
2015-06-03 18:06:01 +0100

    Add Charge GETs

--- lib/BFPHPClient/Entities/Subscription/SubscriptionCharge.php
+++ lib/BFPHPClient/Entities/Subscription/SubscriptionCharge.php
@@ -29,6 +29,57 @@ class Bf_SubscriptionCharge extends Bf_MutableEntity {
 	}
 
 	/**
+	 * Gets Bf_SubscriptionCharges for a given Bf_Account
+	 * @param union[string | Bf_Account] $account Reference to account <string>: $id of the Bf_Account. <Bf_Account>: The Bf_Account entity.
+	 * @return Bf_SubscriptionCharge[]
+	 */
+	public static function getForAccount($account, $options = NULL, $customClient = NULL) {
+		$accountID = Bf_Account::getIdentifier($account);
+
+		$endpoint = sprintf("account/%s",
+			rawurlencode($accountID)
+			);
+		
+		return static::getCollection($endpoint, $options, $customClient);
+	}
+
+	/**
+	 * Gets Bf_SubscriptionCharges for a given state
+	 * @param string_ENUM['Voided', 'Pending', 'AwaitingPayment', 'Paid', 'Failed'] State upon which to search
+	 * @return Bf_SubscriptionCharge[]
+	 */
+	public static function getByState($state, $options = NULL, $customClient = NULL) {
+		// empty IDs are no good!
+		if (!$state) {
+    		throw new Bf_EmptyArgumentException("Cannot lookup unspecified state!");
+		}
+
+		$endpoint = sprintf("state/%s",
+			rawurlencode($state)
+			);
+
+		return static::getCollection($endpoint, $options, $customClient);
+	}
+
+	/**
+	 * Gets Bf_SubscriptionCharge for a given version ID
+	 * @param string version ID of the Bf_SubscriptionCharge
+	 * @return Bf_SubscriptionCharge
+	 */
+	public static function getByVersionID($chargeVersionID, $options = NULL, $customClient = NULL) {
+		// empty IDs are no good!
+		if (!$chargeVersionID) {
+    		throw new Bf_EmptyArgumentException("Cannot lookup empty chargeVersionID!");
+		}
+
+		$endpoint = sprintf("version/%s",
+			rawurlencode($chargeVersionID)
+			);
+
+		return static::getFirst($endpoint, $options, $customClient);
+	}
+
+	/**
 	 * Synchronously recalculates the charge.
 	 * @param array $recalculationOptions (Default: All keys set to their respective default values) Encapsulates the following optional parameters:
 	 *	* @param string (Default: NULL) $..['name'] The new name of the charge.
