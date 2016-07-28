6b7bb678299445a60aaea6bb707ff03688c8f584
Alex Birch <alexander.birch@billforward.net>
2016-07-28 12:19:03 +0100

    support optional field upon subscription, which is filled in by (for example) subscription revive

---
+++ lib/BFPHPClient/Entities/SyncResponses/ExecutionReceiptResponse.php
@@ -0,0 +1,15 @@
+<?php
+
+class Bf_ExecutionReceiptResponse extends Bf_BillingEntity {
+	public static function getByID($id, $options = NULL, $customClient = NULL) {
+		throw new Bf_UnsupportedMethodException('Get by ID support is denied for this entity; '
+		 .'at the time of writing, no API endpoint exists to support it.'
+		 .'The entity can be GETted through cascade only (i.e. GET a related entity).');
+	}
+
+	public static function getAll($options = NULL, $customClient = NULL) {
+		throw new Bf_UnsupportedMethodException('Get All support is denied for this entity; '
+		 .'at the time of writing, no API endpoint exists to support it.'
+		 .'The entity can be GETted through cascade only (i.e. GET a related entity).');
+	}
+}
